911ec6b623712992e96cfd5c238b042e9da76e62
matias9garcia <matiasgarciasaldes@gmail.com>
2022-12-06 11:25:58 -0300

    update showIframe.js
    create array of options
    update logic in changing style of html elements
    fix spacing with br

--- assets/js/showIframe.js
+++ assets/js/showIframe.js
@@ -1,24 +1,56 @@
 function showIframe(id) {
-    let iframe;
-    let btn;
 
-    btn = document.getElementById(id);
+    const options = [
+        {
+            "id": "rsoto",
+            "btn": "showIframeRsoto",
+            "iframe": "iframe_rsoto",
+        },
+        {
+            "id": "induserve",
+            "btn": "showIframeInduserve",
+            "iframe": "iframe_induserve",
+        }
+    ];
 
-    if( id == "showIframeRsoto" ){
-        iframe = document.getElementById("iframe_rsoto");
+    function search(id, options){
+
+        for (let i=0; i < options.length; i++) {
+            if (options[i].btn === id) {               
+                return options[i];
+            }
+        }
     }
 
-    else if( id == "showIframeInduserve" ){
-        iframe = document.getElementById("iframe_induserve");
+    function change_style(selection, options){
+        for (let i=0; i < options.length; i++) {
+
+            let iframe = document.getElementById(options[i].iframe);           
+            let btn = document.getElementById(options[i].btn);
+
+            if (options[i] === selection) {    
+
+                if (iframe.style.display === "none"){
+                    iframe.style.display = "inline";
+                    btn.innerHTML = "Ocultar";
+                }
+                else if(iframe.style.display === "inline"){
+                    iframe.style.display = "none";
+                    btn.innerHTML = "Mostrar";
+                }
+            }
+            else {
+                btn.innerHTML = "Mostrar";
+                iframe.style.display = "none";
+            }
+        }
     }
 
-    if (iframe.style.display === "none"){
-        iframe.style.display = "inline";
-        btn.innerHTML = "Ocultar";
+    // start
 
-    } else if(iframe.style.display === "inline"){
-        iframe.style.display = "none";
-        btn.innerHTML = "Mostrar";
-    }
+    let selection = search(id, options);
+    change_style(selection, options);
+
+    // end
     
 }
